fa714a1e9e7c11d598db89b3a87ef8fee55e2735
stevenma1314 <57720654+stevenma1314@users.noreply.github.com>
2019-12-08 23:30:30 -0500

    first change

--- LinqTest/Program.cs
+++ LinqTest/Program.cs
@@ -10,7 +10,7 @@ namespace LinqTest
     {
         static void Main(string[] args)
         {
-            int[] fakedata = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+            int[] fakedata = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,14,15 };
             //GetOdd(fakedata);
             getoddbyfoloop(fakedata);
 
